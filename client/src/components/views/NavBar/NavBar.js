269e89b2a111140fe042d14f24a9957eebce239b
ianlee92 <ethian9227@gmail.com>
2021-01-25 14:30:02 +0900

    Nav 완성

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import './NavBar.scss';
 import axios from 'axios';
-
 function NavBar(props) {
 
-    const onClickHandler = () => {
+    const logoutHandler = () => {
         axios(`/api/users/logout`)
             .then(response => {
                 if(response.data.success) {
@@ -18,11 +17,31 @@ function NavBar(props) {
     return (
         <div>
             <nav>
-                <ul class="nav-container">
-                    <li class="nav-item"><img src='https://www.macworld.co.uk/cmsdata/features/3792941/apple_music_logo_thumb1200_4-3.jpg' /></li>
-                    <li class="nav-item">로그인</li>
-                    <li class="nav-item" onClick={onClickHandler}>로그아웃</li>
-                </ul>
+                <div class="site-division-background">
+                        <div class="column-header-left">
+                            
+                        </div>
+                        <div class="column-header-center">
+                            <div class="column-header-center-logo">
+                                <a class="headerLogo" href="/"><img src='/logo.png' alt='logo' style={{ width:150, height:100}} /></a>
+                            </div>
+                            <div class="column-header-center-menu">
+                                <ul class="nav-container">
+                                    <li class="nav-item">HOME</li>
+                                    <li class="nav-item">NEW</li>
+                                    <li class="nav-item">CD</li>
+                                    <li class="nav-item">LP</li>
+                                    <li class="nav-item">TURNTABLES</li>
+                                    <li class="nav-item">MAGAZINE</li>
+                                    <li class="nav-item">SALE</li>
+                                    <li class="nav-item" onClick={logoutHandler}>Logout</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="column-header-right">
+                            
+                        </div>
+                </div>
             </nav>
         </div>
     )
