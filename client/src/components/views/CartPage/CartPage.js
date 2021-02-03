8b740a51f1e02b17b155dc597a5a7d3e5f6353ab
ianlee92 <ethian9227@gmail.com>
2021-02-03 21:02:20 +0900

    cart calculateTotal

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import { useDispatch } from 'react-redux';
 import { getCartItems } from '../../../_actions/user_action';
 import UserCardBlock from './Sections/UserCardBlock';
@@ -6,7 +6,7 @@ import UserCardBlock from './Sections/UserCardBlock';
 function CartPage(props) {
 
     const dispatch = useDispatch();
-    
+    const [Total, setTotal] = useState(0)
     useEffect(() => {
         let cartItems = []
 
@@ -16,16 +16,28 @@ function CartPage(props) {
                     cartItems.push(item.id)
                 })
                 dispatch(getCartItems(cartItems, props.user.userData.cart))
+                .then(response => { calculateTotal(response.payload) })
             }
         }
     }, [props.user.userData])
     
+    let calculateTotal = (cartDetail) => {
+        let total = 0;
+        
+        cartDetail.map(item => {
+            total += parseInt(item.price,10) * item.quantity
+        })
+        setTotal(total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+        
+    }
     return (
         <div style={{ width: '85%', margin: '3rem auto'}}>
             <div>
                 <UserCardBlock products={props.user.cartDetail}/>
             </div>
-            
+            <div style ={{marginTop: '3rem'}}>
+                <h2> 합계: {Total}원</h2>
+            </div>
         </div>
     )
 }
