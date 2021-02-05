dece3d84724db5bf58f2754fef90414de7512a28
ianlee92 <ethian9227@gmail.com>
2021-02-05 19:57:03 +0900

    cart popup

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -1,11 +1,10 @@
 import React, {useEffect, useState} from 'react';
-import {useDispatch} from 'react-redux';
-import {addToCart} from '../../../_actions/user_action';
 import axios from 'axios';
 import './DetailProductPage.scss';
-import QandA from '../../Util/QandA';
+import QandA from './Sections/QandA';
+import Description from './Sections/Description'
 import { withRouter } from 'react-router-dom';
-import Cart from '../CartPage/Sections/Cart';
+import Cart from './Sections/Cart';
 
 function DetailProductPage(props) {
 
@@ -57,37 +56,7 @@ function DetailProductPage(props) {
                         <div className="productDescriptionSubject"><span className="productDescriptionTitle">상세정보</span></div>
                         <div className="productDescription">{Product.description}</div>
                     </div>
-                    <div className="deliveryDescription">
-                        <hr className="descriptionHr"></hr><br /><br /><br />
-                        ☀︎ 배송기간<br />
-                        <span className="subDescription"><br />
-                        ・  주문 제작 제품으로 영업일 기준 배송완료까지 7일 정도 소요됩니다.<br /><br />
-                        </span><br />
-                        ☀︎ 구매 시 주의사항<br /><br />
-                        <span className="subDescription">
-                        ・ 제품 사용에 문제가 있을 경우 교환/환불해드립니다.<br />
-                        ・ 주문 제작 제품으로 단순 변심에 의한 교환/환불은 불가합니다.<br />
-                        ・ 모니터의 사양에 따라 컬러가 상이할 수 있습니다.<br />
-                        </span>
-                        <br /><br />
-                        ☀︎ 반품/교환 안내<br /><br />
-                        <span className="subDescription">
-                        ・ 반품배송비 : 편도 3000원<br />
-                        ・ 교환배송비 : 6000원<br />
-                        </span>
-                        <br /><br />
-                        ☀︎ 반품/교환 사유에 따른 요청 가능 기간<br /><br />
-                        <span className="subDescription">
-                        ・ 상품하자의 경우 상품 수령 후 5일 이내 (판매자 반품배송비 부담)</span><br />
-                        <br /><br />
-                        ☀︎ 반품/교환 불가능 사유<br /><br />
-                        <span className="subDescription">
-                        ・ 반품요청기간이 지난 경우<br />
-                        ・ 구매자의 책임 있는 사유로 상품 등이 훼손된 경우 (상품의 내용을 확인하기 위하여 포장을 훼손한 경우는 제외)<br />
-                        ・ 구매자의 사용에 의하여 상품의 가치가 감소한 경우<br />
-                        ・ 시간의 경과에 의하여 재판매가 곤란할 정도로 상품의 가치가 감소한 경우<br />
-                        ・ 고객주문 확인 후 상품제작에 들어가는 주문제작상품</span><br />
-                    </div>
+                    <Description />
                     <QandA />
                 </div>
             </div>
--- client/src/components/views/DetailProductPage/Sections/Cart.js
+++ client/src/components/views/DetailProductPage/Sections/Cart.js
@@ -2,13 +2,19 @@ import React, {useEffect, useState} from 'react';
 import {useDispatch} from 'react-redux';
 import {addToCart} from '../../../../_actions/user_action';
 import { withRouter } from 'react-router-dom';
+import '../DetailProductPage.scss';
 
 function Cart(props) {
+    const [Alert, setAlert] = useState(false)
+    useEffect(() => {
+        let timer = setTimeout(()=>{setAlert(false)},1300);
+        return ()=>{clearTimeout(timer)}
+    },[Alert]);
 
     const dispatch = useDispatch();
     const clickHandler = () => {
         dispatch(addToCart(props.detail._id, props.quantity))
-        //props.history.push('/user/cart')
+        setAlert(true)
     }
     const clickHandler2 = () => {
         dispatch(addToCart(props.detail._id, props.quantity))
@@ -19,6 +25,12 @@ function Cart(props) {
             <div className="cartDiv">
                 <span className="cartButton"><button className="cartBtn" type="button" onClick={clickHandler2}>구매하기</button></span>
                 <span className="cartButton"><button className="cartBtn2" type="button" onClick={clickHandler}>장바구니에 담기</button></span>
+                { Alert === true ?
+                <div className="cartPopup">
+                    <div className="cartPopupContent" style={{paddingTop:'13px'}}>🛒 장바구니에 상품을 담았습니다.</div>
+                </div>
+                : null    
+                }
             </div>
     )
 }
