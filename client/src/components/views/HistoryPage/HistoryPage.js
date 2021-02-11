bce83f7c3fe6f9dc1e4ed1db214dbec9f0401f56
ianlee92 <ethian9227@gmail.com>
2021-02-11 15:12:44 +0900

    구매내역

--- client/src/components/views/HistoryPage/HistoryPage.js
+++ client/src/components/views/HistoryPage/HistoryPage.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import './HistoryPage.scss';
 
-function HistoryPage() {
+function HistoryPage(props) {
     return (
         <div className="historyHead" style={{marginTop:'220px'}}>
             <h2>마이페이지</h2>
-            
+            <table>
+                <thead>
+                    <tr>
+                        <th>Payment Id</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Date of Purchase</th>
+                    </tr>
+                </thead>
+                    {props.user.userData && 
+                        props.user.userData.history.map(item => (
+                            <tr key={item.id}>
+                                <td>{item.id}</td>
+                                <td>{item.price}</td>
+                                <td>{item.quantity}</td>
+                                <td>{item.dateOfPurchase}</td>
+                            </tr>
+                        ))}
+                <tbody>
+
+                </tbody>
+            </table>
         </div>
     )
 }
